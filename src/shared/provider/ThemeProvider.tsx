129350ca2ee3b426f671cbc095e3a7121fb51576
hyunaeri <tjgus9650@gmail.com>
2025-03-19 19:43:05 +0900

    feat: 라이트/다크 모드 전역 상태 관리를 위한 Provider 구현

---
+++ src/shared/provider/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { useDarkMode } from "@/src/widgets/theme-toggle/model/store";
+
+export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const darkMode = useDarkMode();
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  return <>{children}</>;
+}
