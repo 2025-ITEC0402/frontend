2bbbe30a1eeacd3adbfa7fe04a958a957ad8d5d3
hyunaeri <tjgus9650@gmail.com>
2025-03-19 19:54:59 +0900

    refactor: 사용하지 않는 import문 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import { ThemeToggleButton, useDarkMode } from '@/src/widgets/theme-toggle';
+import { ThemeToggleButton } from '@/src/widgets/theme-toggle';
 
 export default function Home() {
-  const darkMode = useDarkMode();
-
   return (
     <>
       <ThemeToggleButton />
