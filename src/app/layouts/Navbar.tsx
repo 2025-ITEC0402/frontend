ca9bccb09cc66f4e6cdf54b4f7c2a64c08eb821a
hyunaeri <tjgus9650@gmail.com>
2025-03-28 01:16:17 +0900

    feat: Navigation component 구현

--- src/app/layouts/Navbar.tsx
+++ src/app/layouts/Navbar.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import darkLogo from '@/public/images/darkLogo.png';
 import lightLogo from '@/public/images/lightLogo.png';
 import { ThemeToggleButton } from '@/src/widgets/theme-toggle';
+import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -16,13 +18,27 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <nav className='w-full border-b border-dashed border-gray-300 bg-white'>
-      <div className='mx-auto flex h-12 max-w-7xl items-center justify-between px-4'>
+    <nav className='w-full border-b border-dashed border-gray-300 bg-white dark:border-gray-700 dark:bg-[var(--background)]'>
+      <div className='mx-auto flex h-16 max-w-7xl items-center justify-between px-4'>
         <div className='flex items-center gap-2'>
-          <Image src={lightLogo} alt='logo' width={32} height={32} />
+          <Link href='/' className='transition-transform duration-200 ease-in-out hover:scale-110'>
+            <Image
+              src={lightLogo}
+              alt='Light Logo'
+              width={32}
+              height={32}
+              className='block dark:hidden'
+            />
+            <Image
+              src={darkLogo}
+              alt='Dark Logo'
+              width={32}
+              height={32}
+              className='hidden dark:block'
+            />
+          </Link>
         </div>
 
-        {/* 메뉴 영역 */}
         <div className='flex items-center gap-8'>
           {menuItems.map(({ label, href }) => {
             const isActive = pathname.startsWith(href);
@@ -30,7 +46,12 @@ export default function Navbar() {
               <Link
                 key={href}
                 href={href}
-                className={`text-sm ${isActive ? 'font-bold text-black' : 'text-gray-400'}`}
+                className={clsx(
+                  'text-sm transition-all duration-200 ease-in-out',
+                  isActive
+                    ? 'font-semibold text-black dark:text-white'
+                    : 'text-gray-400 hover:scale-[1.05] hover:font-medium hover:text-black dark:text-gray-500 dark:hover:text-white',
+                )}
               >
                 {label}
               </Link>
