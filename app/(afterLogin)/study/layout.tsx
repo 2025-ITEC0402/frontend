0ed8819472a8b8eed3541174e76a93754d8acd96
hyunaeri <tjgus9650@gmail.com>
2025-04-04 23:56:40 +0900

    feat: 단원 학습 페이지 임시 구현

--- app/(afterLogin)/study/layout.tsx
+++ app/(afterLogin)/study/layout.tsx
@@ -3,11 +3,11 @@ import { ReactNode } from 'react';
 
 export default function StudyLayout({ children }: { children: ReactNode }) {
   return (
-    <div className='mx-auto flex min-h-screen max-w-7xl pt-8'>
-      <div className='pt-6'>
+    <div className='relative mx-auto flex min-h-screen max-w-7xl pt-8'>
+      <div className='sticky h-fit top-1/2 -translate-y-1/2'>
         <ChapterSidebar />
       </div>
-      <main className='ml-8 flex-1 p-6'>{children}</main>
+      <main className='ml-8 flex-1'>{children}</main>
     </div>
   );
 }
