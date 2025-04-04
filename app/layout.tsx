360b2f530acbacc61383c68c4b046fc54e8373fb
hyunaeri <tjgus9650@gmail.com>
2025-04-04 15:49:50 +0900

    feat: 루트 레이아웃에 QueryProvider 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
-import Navbar from '@/src/app/layouts/Navbar';
+import QueryProvider from '@/src/shared/provider/QueryProvider';
 import ThemeProvider from '@/src/shared/provider/ThemeProvider';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'EMA',
@@ -22,10 +11,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`antialiased`}>
         <ThemeProvider>
-          <Navbar />
-          {children}
+          <QueryProvider>{children}</QueryProvider>
         </ThemeProvider>
       </body>
     </html>
