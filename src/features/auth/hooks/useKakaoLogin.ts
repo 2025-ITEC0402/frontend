ccf38d587689c630ecd6ea6ef58eb5396e146aad
hyunaeri <tjgus9650@gmail.com>
2025-04-08 20:04:07 +0900

    refactor: useRef 를 사용해 reactStrictMode 환경에서도 useEffect 내 API 중복 호출 방지

--- src/features/auth/hooks/useKakaoLogin.ts
+++ src/features/auth/hooks/useKakaoLogin.ts
@@ -1,11 +1,22 @@
 'use client';
 
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
 import { requestKakaoLogin } from '../model/requestKakaoLogin';
 
-export const useKakaoLogin = () => {
+interface KakaoLoginResponse {
+  accessToken: string;
+}
+
+export const useKakaoLogin = (
+  options: UseMutationOptions<
+    KakaoLoginResponse, // 성공 타입
+    unknown, // 에러 타입
+    string // 변수 타입
+  >,
+) => {
   return useMutation({
     mutationKey: ['kakaoLogin'],
     mutationFn: (code: string) => requestKakaoLogin(code),
+    ...options,
   });
 };
