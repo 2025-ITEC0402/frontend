fc2a8bd1090e1b452af8ea0d8825f9a63233af8e
hyunaeri <tjgus9650@gmail.com>
2025-06-04 21:44:32 +0900

    refactor: QueryClient 외부 사용 가능하도록 분리

--- src/shared/provider/QueryProvider.tsx
+++ src/shared/provider/QueryProvider.tsx
@@ -2,19 +2,19 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ReactNode, useState } from 'react';
+import { ReactNode } from 'react';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: 1, // 실패 시 자동 재시도 횟수
-        refetchOnWindowFocus: false, // 창에 다시 포커스 시 데이터 리페치 방지
-        staleTime: 1000 * 60 * 5, // 데이터 신선도 유지 시간
-      }
-    }
-  }));
-
   return (
     <QueryClientProvider client={queryClient}>
       {children}
