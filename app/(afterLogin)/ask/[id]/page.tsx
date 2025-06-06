516cf64b86c2d129f1f8f0efc413eb764717733f
hyunaeri <tjgus9650@gmail.com>
2025-06-06 19:36:55 +0900

    feat: 채팅방 상세 조회 및 렌더링 페이지 구현 (AskMainPage)

--- app/(afterLogin)/ask/[id]/page.tsx
+++ app/(afterLogin)/ask/[id]/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 
+import { useFetchChatroomDetail } from '@/src/features/ask/api/useFetchChatroomDetail';
 import { AskChat } from '@/src/features/ask/ui/AskChat';
+import { useParams } from 'next/navigation';
 
 export default function AskMainPage() {
+  const params = useParams();
+  const chatRoomId = Number(params.id);
+
+  const { data, isLoading, isError } = useFetchChatroomDetail(chatRoomId);
+
+  if (isLoading) return <div>로딩중...</div>;
+  if (isError || !data) return <div>채팅방 정보를 불러올 수 없습니다.</div>;
+
   return (
-    <>
-      <main className='flex h-full flex-1 flex-col p-8'>
-        <AskChat />
-      </main>
-    </>
+    <main className='flex h-full flex-1 flex-col p-8'>
+      <AskChat chatroom={data} />
+    </main>
   );
 }
